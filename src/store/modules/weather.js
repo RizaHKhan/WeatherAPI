3323952e346dde9b75012e8fcd42299de8631e0b
Riza Khan <khanriza@gmail.com>
2019-10-24 13:51:44 -0400

    secured resources

--- src/store/modules/weather.js
+++ src/store/modules/weather.js
@@ -16,7 +16,7 @@ const mutations = {
 
 const actions = {
     act_postalcode({dispatch, commit}, payload) {
-        fetch('http://api.geonames.org/postalCodeLookupJSON?postalcode=' + payload +'&country=US&username=mastaraz', {
+        fetch('https://api.geonames.org/postalCodeLookupJSON?postalcode=' + payload +'&country=US&username=mastaraz', {
             method: 'GET'}
         )
         .then(res => res.json())
@@ -28,7 +28,7 @@ const actions = {
         })
     },
     act_weather({commit}, payload) {
-        fetch('http://api.geonames.org/findNearByWeatherJSON?lat=' + payload.postalcodes[0].lat + '&lng=' + payload.postalcodes[0].lng + '&username=mastaraz', {
+        fetch('https://api.geonames.org/findNearByWeatherJSON?lat=' + payload.postalcodes[0].lat + '&lng=' + payload.postalcodes[0].lng + '&username=mastaraz', {
             method: 'GET'
         })
         .then(res => res.json())
